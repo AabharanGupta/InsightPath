716852ea6f0166c63d141ab4442ef446b5cba0f9
Aabharan <aabharangupta1001@gmail.com>
2025-07-31 17:40:38 +0530

    check

--- client/src/components/ContentCard.jsx
+++ client/src/components/ContentCard.jsx
@@ -5,6 +5,8 @@ const ContentCard=({content})=>{
     const API_URL =import.meta.env.VITE_API_URL;
     return(
         <Link to={`${API_URL}/content/${content._id}`} className={styles.card}>
+        console.log(`${process.env.REACT_APP_API_URL}/api/content`);
+
         <h3>{content.title}</h3>
         <p>{content.description.substring(0,100)}</p>
         <p className={styles.author}>By: {content.author?.name || 'Anonymous'}</p>
