b12589384c0764e9a252df93e7d32a803dbfcb8d
Aabharan <aabharangupta1001@gmail.com>
2025-07-31 18:48:42 +0530

    fix

--- client/src/components/ContentCard.jsx
+++ client/src/components/ContentCard.jsx
@@ -4,7 +4,7 @@ import styles from './ContentCard.module.css'
 const ContentCard=({content})=>{
     const API_URL=import.meta.env.VITE_API_URI_WITHOUT_API
     return(
-        <Link to={`${API_URL}/content/${content._id}`} className={styles.card}>
+        <Link to={`${API_URL}/api/content/${content._id}`} className={styles.card}>
 
         <h3>{content.title}</h3>
         <p>{content.description.substring(0,100)}</p>
