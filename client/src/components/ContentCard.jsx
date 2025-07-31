09fb6eb0b7029f1099e7de6714a2da1b2e820c5b
Aabharan <aabharangupta1001@gmail.com>
2025-07-31 18:46:35 +0530

    ag

--- client/src/components/ContentCard.jsx
+++ client/src/components/ContentCard.jsx
@@ -2,8 +2,9 @@ import {Link} from 'react-router-dom'
 import styles from './ContentCard.module.css'
 
 const ContentCard=({content})=>{
+    const API_URL=import.meta.env.VITE_API_URI_WITHOUT_API
     return(
-        <Link to={`/content/${content._id}`} className={styles.card}>
+        <Link to={`${API_URL}/content/${content._id}`} className={styles.card}>
 
         <h3>{content.title}</h3>
         <p>{content.description.substring(0,100)}</p>
